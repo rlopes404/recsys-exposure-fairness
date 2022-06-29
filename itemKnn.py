cee766aa698419f469cbbba877e8813aa7558a00
rlopes404 <ramonp88@gmail.com>
2022-06-28 21:25:06 -0300

    fixing center

--- itemKnn.py
+++ itemKnn.py
@@ -6,11 +6,13 @@ class ItemKnn():
     def __init__(self, nnbrs, min_nbrs, train, items):
         super(ItemKnn, self).__init__()
 
-        self.train = train.rename(columns={'user_id':'user', 'item_id':'item'})        
-        self.algo = knn.ItemItem(nnbrs, min_nbrs=min_nbrs)
+        self.train = train.rename(columns={'user_id':'user', 'item_id':'item'}).drop(columns=['group'])
+        self.algo = knn.ItemItem(nnbrs, min_nbrs=min_nbrs, center=False)
         self.algo.fit(self.train)
         self.items = items
 
     def full_predict(self, user_id):
+        # print(user_id)
+        # print(self.items)
         predictions = self.algo.predict_for_user(user_id, self.items)
         return np.nan_to_num(predictions)
